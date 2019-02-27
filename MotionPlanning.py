1ca220ec86ac28afdd276dcf775220acc6a62a18
kevinko1788 <kevinko1788@gmail.com>
2019-02-27 11:44:03 -0800

    Dynamic Programming

--- MotionPlanning.py
+++ MotionPlanning.py
@@ -1,34 +1,12 @@
 # ----------
 # User Instructions:
 # 
-# Define a function, search() that returns a list
-# in the form of [optimal path length, row, col]. For
-# the grid shown below, your function should output
-# [11, 4, 5].
+# Create a function compute_value which returns
+# a grid of values. The value of a cell is the minimum
+# number of moves required to get from the cell to the goal. 
 #
-# If there is no valid path from the start point
-# to the goal, your function should return the string
-# 'fail'
-# ----------
-
-# Grid format:
-#   0 = Navigable space
-#   1 = Occupied space
-
-#astar
-# -----------
-# User Instructions:
-#
-# Modify the the search function so that it becomes
-# an A* search algorithm as defined in the previous
-# lectures.
-#
-# Your function should return the expanded grid
-# which shows, for each element, the count when
-# it was expanded or -1 if the element was never expanded.
-# 
-# If there is no path from init to goal,
-# the function should return the string 'fail'
+# If a cell is a wall or it is impossible to reach the goal from a cell,
+# assign that cell a value of 99.
 # ----------
 
 grid = [[0, 1, 0, 0, 0, 0],
@@ -36,15 +14,8 @@ grid = [[0, 1, 0, 0, 0, 0],
         [0, 1, 0, 0, 0, 0],
         [0, 1, 0, 0, 0, 0],
         [0, 0, 0, 0, 1, 0]]
-heuristic = [[9, 8, 7, 6, 5, 4],
-             [8, 7, 6, 5, 4, 3],
-             [7, 6, 5, 4, 3, 2],
-             [6, 5, 4, 3, 2, 1],
-             [5, 4, 3, 2, 1, 0]]
-
-init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
-cost = 1
+cost = 1 # the cost associated with moving from a cell to an adjacent one
 
 delta = [[-1, 0 ], # go up
          [ 0, -1], # go left
@@ -53,54 +24,11 @@ delta = [[-1, 0 ], # go up
 
 delta_name = ['^', '<', 'v', '>']
 
-def search(grid,init,goal,cost,heuristic):
+def compute_value(grid,goal,cost):
     # ----------------------------------------
-    # modify the code below
+    # insert code below
     # ----------------------------------------
-    closed = [[0 for col in range(len(grid[0]))] for row in range(len(grid))]
-    closed[init[0]][init[1]] = 1
-
-    expand = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]
-    action = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]
-
-    x = init[0]
-    y = init[1]
-    g = 0
-    h = heuristic[x][y]
-    f = g+h
-
-    open = [[f, g, h, x, y]]
-
-    found = False  # flag that is set when search is complete
-    resign = False # flag set if we can't find expand
-    count = 0
     
-    while not found and not resign:
-        if len(open) == 0:
-            resign = True
-            return "Fail"
-        else:
-            open.sort()
-            open.reverse()
-            next = open.pop()
-            x = next[3]
-            y = next[4]
-            g = next[1]
-            expand[x][y] = count
-            count += 1
-            
-            if x == goal[0] and y == goal[1]:
-                found = True
-            else:
-                for i in range(len(delta)):
-                    x2 = x + delta[i][0]
-                    y2 = y + delta[i][1]
-                    if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
-                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
-                            g2 = g + cost
-                            h2 = heuristic[x2,y2]
-                            f2 = g2 + h2
-                            open.append([f2, g2, h2, x2, y2])
-                            closed[x2][y2] = 1
-
-    return expand
+    # make sure your function returns a grid of values as 
+    # demonstrated in the previous video.
+    return value 
